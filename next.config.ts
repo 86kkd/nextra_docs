1b2c5be3157e684d0003c4c05ff1de7f33a63ef3
86kkd <2973720373@qq.com>
2025-07-17 00:38:18 +0800

    feat: add copy buttum and fix pagefind mistake

--- next.config.ts
+++ next.config.ts
@@ -2,15 +2,11 @@ import nextra from "nextra";
 
 // Set up Nextra with its configuration
 const withNextra = nextra({
-  // 使用Nextra的默认配置
+  defaultShowCopyCode: true,
+  unstable_shouldAddLocaleToLinks: true,
 });
 
 // Export the final Next.js config with Nextra included
 export default withNextra({
-  output: "export",
-  turbopack: {
-    resolveAlias: {
-      "next-mdx-import-source-file": "src/mdx-components.js",
-    },
-  },
+  reactStrictMode: true,
 });
