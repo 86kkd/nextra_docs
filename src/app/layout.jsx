3d51e89e030acc3a07a90a4dcb6e67780edec096
86kkd <2973720373@qq.com>
2025-08-18 00:20:12 +0800

    feat: delete banner

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -16,7 +16,7 @@ export const metadata = {
   }
 };
 
-const banner = <Banner storageKey="some-key">Nextra 4.0 is released 🎉</Banner>;
+// const banner = <Banner storageKey="some-key">Nextra 4.0 is released 🎉</Banner>;
 const navbar = (
   <Navbar
     logo={<b>Nextra</b>}
@@ -45,7 +45,7 @@ export default async function RootLayout({ children }) {
       </Head>
       <body suppressHydrationWarning>
         <Layout
-          banner={banner}
+          // banner={banner}
           navbar={navbar}
           pageMap={await getPageMap()}
           docsRepositoryBase="https://github.com/shuding/nextra/tree/main/docs"
